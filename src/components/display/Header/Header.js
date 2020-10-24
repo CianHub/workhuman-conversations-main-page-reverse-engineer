69d21772a5b9d268435795898a373d3cf4296e3a
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-24 23:34:34 +0100

    Add header profile

--- src/components/display/Header/Header.js
+++ src/components/display/Header/Header.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import RoundedImage from '../RoundedImage/RoundedImage';
 
 import './Header.css';
 
 const Header = (props) => {
-  return <header className="app-content-header">Header</header>;
+  return (
+    <header className="app-content-header">
+      <div className="app-content-header-profile">
+        <RoundedImage
+          src={
+            'https://img4.wikia.nocookie.net/__cb20080815045819/starwars/images/thumb/7/73/Chewbaccaheadshot.jpg/500px-Chewbaccaheadshot.jpg'
+          }
+          alt={'avatar'}
+          width={'30px'}
+          height={'30px'}
+        />
+        <div className="app-content-header-profile-text">
+          <span className="app-content-header-profile-text-viewing">
+            Viewing
+          </span>
+          <span className="app-content-header-profile-text-username">
+            Chewbacca
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 Header.propTypes = {};
