6caae9e9bd86af13c11dfc08b78c99d22f66ac70
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-24 20:11:52 +0100

    Add sidenav list

--- src/components/display/RoundedImage/RoundedImage.js
+++ src/components/display/RoundedImage/RoundedImage.js
@@ -3,13 +3,22 @@ import PropTypes from 'prop-types';
 
 import './RoundedImage.css';
 
-const RoundedImage = ({ src, alt }) => {
-  return <img src={src} alt={alt} className="app-rounded-img" />;
+const RoundedImage = ({ src, alt, width, height }) => {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className="app-rounded-img"
+      style={{ height, width }}
+    />
+  );
 };
 
 RoundedImage.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
+  height: PropTypes.string,
+  width: PropTypes.string,
 };
 
 export default RoundedImage;
--- src/components/display/SideBar/SideBarList/SideBarList.js
+++ src/components/display/SideBar/SideBarList/SideBarList.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { CharacterContext } from '../../../../state/CharacterState/CharacterContext';
+import RoundedImage from '../../RoundedImage/RoundedImage';
 
 import './SideBarList.css';
 
@@ -9,8 +10,17 @@ const SideBarList = () => {
   return (
     <div className="app-sidebar-list">
       {characters.characters.map((character) => (
-        <div key={character}>
-          <p>{character}</p>
+        <div key={character} className="app-sidebar-list-item">
+          <RoundedImage
+            src={
+              'https://i.insider.com/5b64303942e1cc4d563ab9f1?width=1800&format=jpeg&auto=webp'
+            }
+            alt={'user'}
+            height={'30px'}
+            width={'30px'}
+            className="app-sidebar-list-item-img"
+          />
+          <span className="app-sidebar-list-item-text">{character}</span>
         </div>
       ))}
     </div>
