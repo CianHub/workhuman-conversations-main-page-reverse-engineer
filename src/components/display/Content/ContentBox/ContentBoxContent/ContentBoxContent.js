9188f5fb01367bdb35541ee4a842f86806d2e1d4
Cian O'Gralaigh <ihatecian@gmail.com>
2020-10-26 15:17:35 +0000

    refactor content box logic to use render props pattern

--- src/components/display/Content/ContentBox/ContentBoxContent/ContentBoxContent.js
+++ src/components/display/Content/ContentBox/ContentBoxContent/ContentBoxContent.js
@@ -1,62 +1,14 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import PropTypes from 'prop-types';
 
-import Profile from '../../../Profile/Profile';
 import { CharacterContext } from '../../../../../state/CharacterState/CharacterContext';
 import './ContentBoxContent.css';
 
-const ContentBoxContent = ({ type, dataItem }) => {
+const ContentBoxContent = ({ dataItem, children }) => {
   const { characters } = useContext(CharacterContext)[0];
-
   const char = characters?.find((char) => char.id === dataItem.authorId);
 
-  if (type === 'Check-ins') {
-    return (
-      <div className="app-content-box">
-        <div className="app-content-box-profile">
-          <Profile character={char} subtitle={'Check-ins'} />
-        </div>
-        <span className="app-content-box-title">{dataItem.title}</span>
-        <p className="app-content-box-message">{dataItem.message}</p>
-        <p className="app-content-box-date">{dataItem.age}</p>
-      </div>
-    );
-  }
-  if (type === 'Feedback') {
-    return (
-      <div className="app-content-box">
-        <div className="app-content-box-profile">
-          <Profile character={char} subtitle={'Feedback'} />
-        </div>
-        <p className="app-content-box-message">{dataItem.message}</p>
-        <p className="app-content-box-date">{dataItem.age}</p>
-      </div>
-    );
-  }
-  if (type === 'Priorities') {
-    return (
-      <div className="app-content-box">
-        {dataItem.type === 'active' ? (
-          <p className="app-content-box-message">My active priorities</p>
-        ) : (
-          <p className="app-content-box-message">My closed priorities</p>
-        )}
-        <ul className="app-content-box-list">
-          {dataItem.tasks.map((task, idx) => (
-            <li key={task + idx}>
-              <input
-                type="checkbox"
-                checked={dataItem.type !== 'active'}
-                readOnly
-              />
-              {task}
-            </li>
-          ))}
-        </ul>
-        <br />
-      </div>
-    );
-  }
+  return children(dataItem, char);
 };
 
 ContentBoxContent.propTypes = {
--- src/components/display/Content/ContentColumn/ContentColumn.js
+++ src/components/display/Content/ContentColumn/ContentColumn.js
@@ -5,13 +5,69 @@ import ContentBoxContent from '../ContentBox/ContentBoxContent/ContentBoxContent
 import ContentBoxHeader from '../ContentBox/ContentBoxHeader/ContentBoxHeader';
 
 import './ContentColumn.css';
+import Profile from '../../Profile/Profile';
+
+const FeedBackContentBox = ({ char, dataItem }) => (
+  <div className="app-content-box">
+    <div className="app-content-box-profile">
+      <Profile character={char} subtitle={'Feedback'} />
+    </div>
+    <p className="app-content-box-message">{dataItem.message}</p>
+    <p className="app-content-box-date">{dataItem.age}</p>
+  </div>
+);
+
+const CheckInContentBox = ({ char, dataItem }) => (
+  <div className="app-content-box">
+    <div className="app-content-box-profile">
+      <Profile character={char} subtitle={'Check-ins'} />
+    </div>
+    <span className="app-content-box-title">{dataItem.title}</span>
+    <p className="app-content-box-message">{dataItem.message}</p>
+    <p className="app-content-box-date">{dataItem.age}</p>
+  </div>
+);
+
+const PrioritiesContentBox = ({ dataItem }) => (
+  <div className="app-content-box">
+    {dataItem.type === 'active' ? (
+      <p className="app-content-box-message">My active priorities</p>
+    ) : (
+      <p className="app-content-box-message">My closed priorities</p>
+    )}
+    <ul className="app-content-box-list">
+      {dataItem.tasks.map((task, idx) => (
+        <li key={task + idx}>
+          <input
+            type="checkbox"
+            checked={dataItem.type !== 'active'}
+            readOnly
+          />
+          {task}
+        </li>
+      ))}
+    </ul>
+    <br />
+  </div>
+);
 
 const ContentColumn = ({ title, color, data }) => {
+  const handleContentBox = (dataItem, char) => {
+    if (title === 'Feedback')
+      return <FeedBackContentBox char={char} dataItem={dataItem} />;
+    if (title === 'Priorities')
+      return <PrioritiesContentBox dataItem={dataItem} />;
+    if (title === 'Check-ins')
+      return <CheckInContentBox char={char} dataItem={dataItem} />;
+  };
+
   return (
     <div className="app-content-column">
       <ContentBoxHeader title={title} color={color} />
       {data.map((item, idx) => (
-        <ContentBoxContent type={title} dataItem={item} key={title + idx} />
+        <ContentBoxContent dataItem={item} key={title + idx}>
+          {(dataItem, char) => handleContentBox(dataItem, char)}
+        </ContentBoxContent>
       ))}
     </div>
   );
@@ -20,7 +76,7 @@ const ContentColumn = ({ title, color, data }) => {
 ContentColumn.propTypes = {
   title: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
-  data: PropTypes.array,
+  data: PropTypes.array.isRequired,
 };
 
 export default ContentColumn;
